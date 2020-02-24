82e99a1c2a2fbe774d2a5bcc39300a71f75635bb
Damien Lecan <damien.lecan@spacelephant.org>
2020-02-24 11:13:17 +0100

    chore: experimental support of anti-tracker tools

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,10 +27,10 @@ module.exports = {
       var _paq = window._paq || [];
       (function() {
         var u="https://kpi.unik-name.com/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setTrackerUrl', u+'r.php']);
         _paq.push(['setSiteId', '5']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'js/'; s.parentNode.insertBefore(g,s);
       })();
     `
     ]
