16bf7cefc7995db9b7febca7e109622ccfd29951
Damien Lecan <damien.lecan@spacelephant.org>
2020-11-04 22:14:47 +0100

    doc: fix broken footer

--- docs/.vuepress/components/Footer.vue
+++ docs/.vuepress/components/Footer.vue
@@ -1,10 +1,13 @@
 <template>
   <footer>
     <p></p>
-    © 2018-2020 <a href="https://www.unikname.com">Unikname</a>&nbsp;&bullet;&nbsp;Secured by <a href="https://uns.network">uns.network</a> blockchain&nbsp;&bullet;&nbsp;<a href="/5-support/powered-by-ark-io/">Powered by ARK</a>
-    <p></p>
+    © 2018-2020
+    <a href="https://www.unikname.com">Unikname</a>&nbsp;&bullet;&nbsp;Secure by
+    <a href="https://uns.network">uns.network</a>
+    blockchain&nbsp;&bullet;&nbsp;<a href="/5-support/powered-by-ark-io/"
+      >Powered by ARK</a
+    >
   </footer>
 </template>
 
-<style lang="stylus" scoped>
-</script>
+<style lang="stylus" scoped></style>
--- docs/.vuepress/components/Needhelp.vue
+++ docs/.vuepress/components/Needhelp.vue
@@ -1,15 +1,14 @@
 <template>
-    <span>
-        <br/>
-        <div class="custom-block tip">
-            <p class="custom-block-title">Need more help?</p>
-            <p>Open a topic on our <Forumurl/></p>
-        </div>
-    </span>
+  <span>
+    <br />
+    <div class="custom-block tip">
+      <p class="custom-block-title">Need help?</p>
+      <p>Open a topic on our <forumurl /></p>
+    </div>
+  </span>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style lang="stylus">
 @require './../theme/styles/wrapper.styl'
