0b488648009bcff96908f6f6c92127599c1a4527
Damien Lecan <damien.lecan@spacelephant.org>
2020-12-10 11:31:46 +0100

    feat: allow to start the chat from the footer

--- docs/.vuepress/components/Needhelp.vue
+++ docs/.vuepress/components/Needhelp.vue
@@ -1,15 +1,39 @@
 <template>
   <span>
-    <br />
     <div class="custom-block tip">
       <p class="custom-block-title">Still need help?</p>
+      <p @click="showRocketWidget">
+        Live-chat with us by clicking on this button on the right:
+        <img class="livechat-icon" src="/livechat-icon.png" />
+      </p>
+      <p>OR</p>
       <p>Open a topic on our <forumurl /></p>
     </div>
   </span>
 </template>
 
-<script></script>
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {
+    showRocketWidget: function() {
+      if (typeof window.RocketChat !== "undefined")
+        window.RocketChat(function() {
+          this.maximizeWidget();
+        });
+    },
+  },
+};
+</script>
 
 <style lang="stylus">
 @require './../theme/styles/wrapper.styl'
+</style>
+
+<style lang="stylus" scoped>
+img.livechat-icon {
+  vertical-align:middle;
+}
 </style>
